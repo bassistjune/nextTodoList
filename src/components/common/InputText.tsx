699b751139aa0437f8c02ec6088b0bf9ae225bf6
bassistjune <bassistjune@gmail.com>
2024-12-05 13:14:54 +0900

    20241205 - todo 등록 개수 및 글자 수 제한 .by 한준희

--- src/components/common/InputText.tsx
+++ src/components/common/InputText.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSetRecoilState } from "recoil";
 import { todoState } from "../../atoms/todoAtom";
+import {placeholder} from "@babel/types";
 
 const TextComponent: React.FC = () => {
     const [value, setValue] = useState("");
@@ -8,11 +9,24 @@ const TextComponent: React.FC = () => {
 
     const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
         if (event.key === "Enter" && value.trim()) {
-            setTodos((prev) => [
-                ...prev,
-                { id: Date.now(), content: value.trim(), isDone: false },
-            ]);
-            setValue("");
+            if (value.length > 20) {
+                alert("Todo 내용은 20자 이내로 작성해야 합니다.");
+                return false;
+            }
+
+           else {
+                setTodos((prev) => {
+                    if (prev.filter((todo) => !todo.isDone).length >= 10) {
+                        alert("완료된 Todo는 최대 10개까지만 가능합니다.");
+                        return prev;
+                    }
+                    return [
+                        ...prev,
+                        { id: Date.now(), content: value.trim(), isDone: false },
+                    ];
+                });
+                setValue("");
+            }
         }
     };
 
@@ -24,6 +38,7 @@ const TextComponent: React.FC = () => {
             onChange={(e) => setValue(e.target.value)}
             onKeyDown={handleKeyDown}
             placeholder="할 일을 입력 해주세요."
+            maxLength={20}
         />
     );
 };
