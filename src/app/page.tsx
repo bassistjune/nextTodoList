080e33616cba144a2b3802eb1d66b7902fecb82a
bassistjune <bassistjune@gmail.com>
2024-12-05 12:59:18 +0900

    20241205 - 컴포넌트, 리코일, 레이아웃등 설정 .by 한준희

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoUserListPage from "../components/pages/TodoUserListPage";
+
+import "../styles/main.scss"
+interface Props {}
+
+const Page = ({}: Props) => {
+  return <TodoUserListPage />;
+};
+
+export default Page;
